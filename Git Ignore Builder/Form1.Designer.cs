e18ca42221b9f0c9c6efa91e837a7d26c8e12592
Or Fleisher <contact@orfleisher.com>
2017-02-26 21:18:26 -0500

    Initial commit

---
+++ Git Ignore Builder/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Git_Ignore_Builder
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateButton = new System.Windows.Forms.Button();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.Enabled = false;
+            this.generateButton.Location = new System.Drawing.Point(318, 154);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(234, 71);
+            this.generateButton.TabIndex = 0;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(47, 154);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(234, 71);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(47, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(505, 37);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // App
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(581, 271);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.generateButton);
+            this.MaximumSize = new System.Drawing.Size(609, 350);
+            this.MinimumSize = new System.Drawing.Size(609, 350);
+            this.Name = "App";
+            this.Text = ".gitignore Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button browseButton;
+        private System.DirectoryServices.DirectoryEntry directoryEntry1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
